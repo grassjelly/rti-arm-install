be09efdeff9691febb66c7311885ddc1ab69305c
Juan Miguel Jimeno <jimenojmm@gmail.com>
2017-05-04 23:03:51 +0800

    fixed .bashrc (revert to last commit)

--- install.sh
+++ install.sh
@@ -15,4 +15,3 @@ sudo apt-get install -y oracle-java8-installer
 echo "export JREHOME=/usr/lib/jvm/java-8-oracle" >> ~/.bashrc
 echo "export LD_LIBRARY_PATH=$HOME/rti_connext_dds-5.2.3/lib/armv6vfphLinux3.xgcc4.7.2:$LD_LIBRARY_PATH" >> ~/.bashrc
 echo "source $HOME/rti_connext_dds-5.2.3/resource/scripts/rtisetenv_armv6vfphLinux3.xgcc4.7.2.bash" >> ~/.bashrc
-echo "alias rti=\"$HOME/rti_connext_dds-5.2.3/resource/scripts/rtisetenv_x64Linux3gcc4.8.2.bash" >> ~/.bashrc
