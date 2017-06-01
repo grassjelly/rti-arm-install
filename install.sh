6b1910757ec918b4cdf4d49e4aca75e1b00b4d1c
Juan Jimeno <jimenojmm@gmail.com>
2017-06-01 02:41:40 +0000

    changed from oracle8 to openjdk8

--- install.sh
+++ install.sh
@@ -5,13 +5,16 @@ echo "https://community.rti.com/content/forum-topic/howto-run-rti-connext-dds-ra
 echo "PRESS [ENTER] TO CONTINUE"
 read
 
-#Install Java8
-sudo apt-get install -y software-properties-common
-sudo apt-get install -y python-software-properties
-sudo add-apt-repository ppa:webupd8team/java
-sudo apt-get update
-sudo apt-get install -y oracle-java8-installer
+#Install oracle-java8
+#sudo apt-get install -y software-properties-common
+#sudo apt-get install -y python-software-properties
+#sudo add-apt-repository ppa:webupd8team/java
+#sudo apt-get update
+#sudo apt-get install -y oracle-java8-installer
 
-echo "export JREHOME=/usr/lib/jvm/java-8-oracle" >> ~/.bashrc
+#install openjdk8
+sudo apt-get install -y openjdk-8-jdk
+
+echo "export JREHOME=/usr/lib/jvm/java-8-openjdk-armhf" >> ~/.bashrc
 echo "export LD_LIBRARY_PATH=$HOME/rti_connext_dds-5.2.3/lib/armv6vfphLinux3.xgcc4.7.2:$LD_LIBRARY_PATH" >> ~/.bashrc
 echo "source $HOME/rti_connext_dds-5.2.3/resource/scripts/rtisetenv_armv6vfphLinux3.xgcc4.7.2.bash" >> ~/.bashrc
